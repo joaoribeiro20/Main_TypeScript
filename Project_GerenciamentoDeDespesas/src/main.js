1d65967183314adb15ecd27aa6d3e03502584597
unknown <jvalves217@gmail.com>
2023-09-19 21:47:48 -0300

    problema com servidor

--- Project_GerenciamentoDeDespesas/src/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var adicionar_1 = require("./adicionar");
-var add = new adicionar_1.Adicionar("joao", 52, "21/10", true);
-console.log(add.validarData("20/14"));
