b3048fed3a81a92259bead62bd07053290431d04
unknown <jvalves217@gmail.com>
2023-09-28 21:01:22 -0300

    atualização clok

--- Project_Clock/dist/timer.js
+++ Project_Clock/dist/timer.js
@@ -6,7 +6,28 @@ let divExibidorInput = document.getElementById('divExibidorTimerInput')
 let divExibiriReal = document.getElementById('divExibidorReal')
 let timerExibirReal = document.getElementById('timerExibirReal')
 let btnTimerIniciar = document.getElementById('btnTimerIniciar')
+let divBotoesTimer1 = document.getElementById('divBotoesTimer1')
+let divBotoesTimer2 = document.getElementById('divBotoesTimer2')
 divTimerPrincipal.removeChild(divExibiriReal)
+
+
+let btnTimerPausar = document.createElement('button')
+btnTimerPausar.setAttribute('class', 'btn')
+btnTimerPausar.setAttribute('id', 'btnPausar')
+btnTimerPausar.innerText = 'Pausar'
+btnTimerPausar.addEventListener('click', pausarTimer)
+/*  */
+let btnTimerRetorna = document.createElement('button')
+btnTimerRetorna.setAttribute('class', 'btn')
+btnTimerRetorna.setAttribute('id', 'btnRetorna')
+btnTimerRetorna.innerText = 'Retorna'
+btnTimerRetorna.addEventListener('click', retornaTimer)
+/*  */
+let btnTimerExcluir = document.createElement('button')
+btnTimerExcluir.setAttribute('class', 'btn')
+btnTimerExcluir.setAttribute('id', 'btnExcluir')
+btnTimerExcluir.innerText = 'Exclui'
+btnTimerExcluir.addEventListener('click', excluirTimer)
 
 let horasTimer 
 let minutosTimer 
@@ -50,7 +71,12 @@ console.log(msg)
 
 if(msg != false){
 divTimerPrincipal.removeChild(divExibidorInput)
+divBotoesTimer1.appendChild(btnTimerPausar)
+divBotoesTimer2.appendChild(btnTimerExcluir)
+
 divTimerPrincipal.appendChild(divExibiriReal)
+divBotoesTimer1.removeChild(btnTimerIniciar)
+
 exibir = true
      tempo = setInterval(() => {
     if(segundosTimer > 0){
