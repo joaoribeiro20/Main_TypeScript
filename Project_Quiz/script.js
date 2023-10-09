4a0d2b712adc5940705dd0de88a9530abadc1b0e
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-10-09 15:31:35 -0300

    v1.0

--- Project_Quiz/script.js
+++ Project_Quiz/script.js
@@ -37,17 +37,3 @@ let perguntas = [
                   ]
                 }
 ]
-let body = document.getElementById('body')
-let divPerguntas = document.createElement("div")
-divPerguntas.setAttribute('class', 'pergunta')
- perguntas[1].questao
-divPerguntas.innerHTML = 
-`${perguntas[1].questao} 
-<hr>
-<br>
-
-<input type="radio" id="2" name="1" value=`${perguntas[1].respostas[0].status}`  />
-  <label for="1">43</label>
-`
-body.appendChild(divPerguntas)
-console.log(perguntas[1].questao)
--- Project_ToDoList/script.js
+++ Project_ToDoList/script.js
@@ -18,6 +18,7 @@
 let inputTarefa = document.querySelector('#inputTarefa')
 let btnADD = document.querySelector('#btnADD')
 let areaTarefa = document.querySelector('#areaTarefa')
+let txtl = []
 
 btnADD.addEventListener('click', ()=>{
     if(inputTarefa.value != ''){
@@ -36,9 +37,60 @@ function excluir(id){
     areaTarefa.removeChild(divExcluida) 
 }
 
-function editar(){
+function editar(txtAtual,local){
+    let inputEditar = document.createElement('input')
+    inputEditar.setAttribute('id', `e${txtAtual}`)
+    inputEditar.setAttribute('class', `divTarefa`)
+    
+     
+    let labelTarefa1 = document.querySelector(`#L${txtAtual}`)
+   
+    console.log(`texto labol: ${txtl[txtAtual]}`)
+    
+   
+
+    let div = document.querySelector(`#i${txtAtual}`)
+    let spanBtns = document.querySelector(`#S${txtAtual}`)
+
+    spanBtns.style.display = 'none'
+    labelTarefa1.style.display= 'none'
+    div.appendChild(inputEditar)
+    
+    inputEditar.value = txtl[txtAtual]
+    inputEditar.focus()
+    let btnSalvar = document.createElement('button')
+    btnSalvar.innerText = `Salvar`
+    btnSalvar.setAttribute('value', `${txtAtual}`)
+    btnSalvar.setAttribute('id', `b${txtAtual}`)
+    btnSalvar.setAttribute('onclick', `teste(${btnSalvar.value})`)
+    div.appendChild(btnSalvar)
+
 
 }
+function teste(n){
+    console.log(n)
+
+    
+
+    let inputEditar = document.querySelector(`#e${n}`)
+    txtl[n] = inputEditar.value
+    let btnSalvar = document.querySelector(`#b${n}`)
+    console.log(inputEditar.value)
+    let div = document.querySelector(`#i${n}`)
+    let labelTarefa1 = document.querySelector(`#L${n}`)
+    let spanBtns = document.querySelector(`#S${n}`)
+
+    div.removeChild(inputEditar)
+    div.removeChild(btnSalvar)
+    labelTarefa1.style.display = 'flex'
+    labelTarefa1.innerText =  txtl[n]
+    spanBtns.style.display = 'flex'
+    
+
+console.log('ebaa')
+}
+
+
 let statusi = true
 function feita(id){
     
@@ -52,7 +104,6 @@ function feita(id){
         let idDiv = `#i${id}`
         let input = document.querySelector(`${idDiv}`)
         input.style.opacity = '100%'
-        
         console.log(input) 
         statusi = true
         
@@ -65,26 +116,35 @@ function createItem(txt){
     let id=idR()
     let div = document.createElement('div')
     div.setAttribute('id', `i${id}`)
+   
     
 
     let inputRadio = document.createElement('input')
     inputRadio.setAttribute('type','radio')
     inputRadio.setAttribute('value', `${id}`)
-    inputRadio.setAttribute('onclick', `feita(${inputRadio.value})`)
+    inputRadio.setAttribute('onclick', `feita(${inputRadio.value},)`)
 
     div.appendChild(inputRadio)
 
     let label = document.createElement('label')
+    label.setAttribute('id', `L${id}`)
+    label.setAttribute('value', `${txt}`)
+    txtl[id]= txt
+    console.log(txtl)
     label.innerText = txt
 
     div.appendChild(label)
 
     let span = document.createElement('span')
+    span.setAttribute('id', `S${id}`)
 
     div.appendChild(span)
 
     let btnEditar = document.createElement('button')
     btnEditar.innerHTML = `<i class="fa-solid fa-pen"></i>`
+    btnEditar.setAttribute('value', `${id}`)
+   
+    btnEditar.setAttribute('onclick', `editar(${btnEditar.value})`)
 
     span.appendChild(btnEditar)
 
@@ -103,5 +163,5 @@ function createItem(txt){
 function idR(){
     //return Math.random();
    // return Math.random() * 20;
-    return parseInt(Math.random() * 20)
+    return parseInt(Math.random() * 100)
 }
